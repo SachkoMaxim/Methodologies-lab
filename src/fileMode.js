819578d94bc067877eb60874e1cf5dbf0617b942
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2024-02-26 16:59:59 +0200

    modified console mode and error validations files Modified validations.js file to validate errors of output file. And used functions of validations.js in file mode file (fileMode.js).

--- src/fileMode.js
+++ src/fileMode.js
@@ -1,4 +1,13 @@
+import { validateInputFilePath, validateOutputFilePath, readMarkdownFile } from './validations.js';
+
 const startFileMode = () => {
+    const inputFilePath = process.argv[2];
+    const outputFilePath = process.argv[4];
+
+    validateInputFilePath(inputFilePath);
+    validateOutputFilePath(outputFilePath);
+
+    const markdownText = readMarkdownFile(inputFilePath);
     console.log("File mode is not implemented yet");
 }
 
